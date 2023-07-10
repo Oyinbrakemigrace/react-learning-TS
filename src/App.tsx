1b8b30171b80c4217247efe9a24c9000d8975f3f
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-07-10 21:15:30 +0400

    React type safety-Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Person, Country } from './Components/Person';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person 
+      name = 'Grace'
+      age = {22}
+      email = 'grace@email.com'
+      isMarried = {false}
+      friends = {['Minty', 'Manty', 'Monty']}
+      country = {Country.Nigeria}
+      />
     </div>
   );
 }
