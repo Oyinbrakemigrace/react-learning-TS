e2c7938397deccaaac122e442bbffd26324eed50
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-07-11 21:44:13 +0400

    React redux tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Person, Country } from './Components/Person';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Contact from './Pages/Contact';
+// import { Person, Country } from './Components/Person';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Store';
 
 
 function App() {
   return (
     <div className="App">
-      <Person 
-      name = 'Grace'
-      age = {22}
-      email = 'grace@email.com'
-      isMarried = {false}
-      friends = {['Minty', 'Manty', 'Monty']}
-      country = {Country.Nigeria}
-      />
+      <Provider store={store}>
+        <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </Router>
+      </Provider>
     </div>
   );
 }
 
 export default App;
+
+
+      
---
+++ src/Pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        Login here
+        <input type="text" />
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Login
